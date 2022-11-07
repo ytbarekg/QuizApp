be5d3eb787f8e4629f89037bb5a126060fa45115
ytbarekg <yhailu@miu.edu>
2022-11-06 21:50:12 -0800

    added part one

--- app/src/main/java/edu/miu/mdp/quizapp/MainActivity.kt
+++ app/src/main/java/edu/miu/mdp/quizapp/MainActivity.kt
@@ -2,10 +2,27 @@ package edu.miu.mdp.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        submit.setOnClickListener{view ->
+            var alertDialog = AlertDialog.Builder(this);
+            alertDialog.setTitle("Result")
+            alertDialog.setMessage(Date().toString() + " \nYour score is 50%")
+            alertDialog.setPositiveButton("Ok") {dialogInterface, which ->
+                dialogInterface.dismiss()
+                finish()
+            };
+            alertDialog.setNegativeButton("Cancel") {dialogInterface, which ->
+                dialogInterface.dismiss()
+            }
+
+            alertDialog.create().show();
+        };
     }
 }
